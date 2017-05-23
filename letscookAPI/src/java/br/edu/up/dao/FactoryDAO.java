c4e1fc72f507bbf322fd768e360b11421cd0bfd3
Henrique Merlin <henmerlin@gmail.com>
2017-05-22 22:19:50 -0300

    commit

---
+++ letscookAPI/src/java/br/edu/up/dao/FactoryDAO.java
@@ -0,0 +1,11 @@
+package br.edu.up.dao;
+
+import br.edu.up.entity.Usuario;
+
+public class FactoryDAO {
+
+    public static InterfaceDAO<Usuario> createUsuarioDAO() {
+        return new UsuarioDAO();
+    }
+
+}
